dea46cd0976d0952da5d432f2d149d8eba9106df
jamesdangbis <dangbisj1@gmail.com>
2024-02-20 10:44:18 +0800

    Linear Search

--- linear_search.py
+++ linear_search.py
@@ -7,31 +7,32 @@ def linear_search(csv_reader, keyword):
             return index, row
     return None, None
 
-
 file_path = r"C:\Users\JAMES\Downloads\words.csv"
 
-
+# Read and concatenate CSV data into a single string
+csv_data_str = ""
 with open(file_path, 'r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)
-
-    print("Values in the CSV file:")
     for row in csv_reader:
-        print(row)
+        csv_data_str += ' '.join(row) + ' '
 
+print("Values in the CSV file after sorting:", csv_data_str)
 
+# Perform linear search
 with open(file_path, 'r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)
-
     keyword = input("\nEnter the keyword to search for: ")
-
     start = time.perf_counter()
     index, result = linear_search(csv_reader, keyword)
-
-    if result:
-        print("Keyword found at index", index, ":", result)
-    else:
-        print("Keyword not found")
     end = time.perf_counter()
-    print("Linear Search Speed: %.6f seconds" % (end - start))
+
+# Print search result
+if result:
+    print("\nKeyword found at index", index, ":", ' '.join(result))
+else:
+    print("\nKeyword not found")
+
+# Print timing
+print("Linear Search Speed: %.6f seconds" % (end - start))
